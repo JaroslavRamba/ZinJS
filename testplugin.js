78680defa2e4614f66f68e250477391def6dc697
Matěj Lang <matejlang@gmail.com>
2012-04-23 20:11:50 +0200

    edit component

--- testplugin.js
+++ testplugin.js
@@ -4,6 +4,8 @@
 new ZinPluginPrototype("testplugin2",ZinPluginType.Component,function Scroll(){
     this.hello=function(){alert("Hello World!")};
 }),
-new ZinPluginPrototype("testplugin2",ZinPluginType.Event,function Scroll(){
-    
+new ZinPluginPrototype("testplugin3",ZinPluginType.Event,function installEvent(prototype){
+    prototype.onmousemove=function(a){
+         prototype.testplugin3(a);
+    }
 })]
