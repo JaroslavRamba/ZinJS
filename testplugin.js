afda61e9cbb407f851f063d3d30b24fde98ac876
Matěj Lang <matejlang@gmail.com>
2012-04-23 06:09:31 +0200

    add zinpluginprototype and some other function zineventplugin is not implemented yet

--- testplugin.js
+++ testplugin.js
@@ -2,7 +2,7 @@
     
 }),
 new ZinPluginPrototype("testplugin2",ZinPluginType.Component,function Scroll(){
-    
+    this.hello=function(){alert("Hello World!")};
 }),
 new ZinPluginPrototype("testplugin2",ZinPluginType.Event,function Scroll(){
     
