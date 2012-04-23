637dc8cde2759da5cc927ff5f6813c21d60de2b4
Matěj Lang <matejlang@gmail.com>
2012-04-23 05:52:00 +0200

    add zinpluginprototype and some other function zineventplugin is not implemented yet

---
+++ testplugin.js
@@ -0,0 +1,9 @@
+[new ZinPluginPrototype("testplugin",ZinPluginType.Action,function(){
+    
+}),
+new ZinPluginPrototype("testplugin2",ZinPluginType.Component,function Scroll(){
+    
+}),
+new ZinPluginPrototype("testplugin2",ZinPluginType.Event,function Scroll(){
+    
+})]
