a6194c4daadee9ae7d5c1afc2243171a813709b5
Jaroslav Ramba <jaroslav@ramba.cz>
2012-05-07 17:43:55 +0200

    Plugin rotate

--- js/plugins/rotation.js
+++ js/plugins/rotation.js
@@ -1,37 +1,24 @@
-new ZinPluginPrototype("rotate",ZinPluginType.Action,function(points, duration, animate){
+new ZinPluginPrototype("rotate",ZinPluginType.Action,function(style){
     
-    
-    /*
-     *
-     * to { 
-    -webkit-transform: rotate(360deg);
-  }
-}
-#loading img
-{
-    -webkit-animation-name:             rotate; 
-    -webkit-animation-duration:         0.5s; 
-    -webkit-animation-iteration-count:  infinite;
-    -webkit-transition-timing-function: linear;
-     *
-     **/
-    
-    
-    this.addCss({
-        position: "absolute",
-        transform: {
-            scale: 2,
-            rotate: "60deg",
-            translate: "20px, 50px"
-        }
-    });
-                
-                
     if (arguments.length == 0) {
+        this.addCss({
+            transform: {
+                rotate: "360deg"
+            },
+            display: "block",
+            height: "100px",
+            width: "100px",
+            transition: "all 1s ease-out",
+        });
                 
                 
     }
-
-    
+    else{
+        if(style instanceof Styles) {
+            this.addCss(style.css);
+        }else{
+            console.log("Object is not instance of Styles");
+        }
+    }
 })
 
